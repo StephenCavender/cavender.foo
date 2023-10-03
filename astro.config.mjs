bd60a09ecbfd7b4b74658b6125b11ebc36ceccea
Steve <github@cavender.io>
2023-10-03 11:23:40 -0400

    only set base if not in dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+const isDev = import.meta.env.DEV;
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stephencavender.github.io',
-	base: '/garden',
+	base: isDev ? undefined : '/garden',
 	integrations: [mdx(), sitemap()],
 });
