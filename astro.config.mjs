bded81e90bfd7e4a9ad046fec5b2cebb4fe047e2
Steve <github@cavender.io>
2023-10-11 16:09:14 -0400

    revert deploy workflow, rm base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,9 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-const isDev = import.meta.env.DEV;
-
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://stephencavender.github.io',
-	base: isDev ? undefined : '/garden',
+	base: '/garden',
 	integrations: [mdx(), sitemap()],
 });
