3af59c2945404b368bb44a8f8d5844a0657c72fe
Steve <github@cavender.io>
2023-10-10 20:23:47 -0400

    remove blog config for article, move articles over

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,21 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const articles = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		publishedDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+		heroImage: z.object({
+			url: z.string(),
+			credit: z.object({
+				name: z.string(),
+				url: z.string(),
+			})
+		}).optional(),
+	})
 });
 
-export const collections = { blog };
+export const collections = { articles };
