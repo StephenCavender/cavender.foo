aa6278cfa97eb9286200596a2a8a6f0896d02a3d
Steve <github@cavender.io>
2026-04-21 10:26:25 -0400

    update title/description

--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,3 @@
-export const SITE_TITLE = "Steve's Site";
-export const SITE_DESCRIPTION = "Personal site of Steve Cavender";
+export const SITE_TITLE = "Steve Cavender";
+export const SITE_DESCRIPTION = "Digital Garden of Steve Cavender";
 export const SITE_URL = "https://cavender.foo";
