96e6d4db75710fd8ab3ea8469a52c43c59c00a2a
Steve <github@cavender.io>
2026-04-25 11:22:59 -0400

    refactor: move content config to src/content.config.ts for Astro 6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
 
 const articles = defineCollection({
   // Type-check frontmatter using a schema
