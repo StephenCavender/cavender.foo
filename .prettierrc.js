039cc3ad7f936ea8f5c16086d50a7a1a97794a36
Steve <github@cavender.io>
2026-08-07 14:44:27 -0400

    update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,9 @@
 /** @type {import("prettier").Config} */
 export default {
   trailingComma: "es5",
+  printWidth: 80,
+  htmlWhitespaceSensitivity: "ignore",
+  bracketSameLine: false,
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
